61064f4f7903ce20f67b0daf20d85f1b46388641
Yauhen Kuzmich <y.kuzmich@godeltech.com>
2021-07-30 11:10:42 +0300

    Some comments

--- bikesharing-backend/src/main/java/com/godeltech/bikesharing/controller/ServiceOperationController.java
+++ bikesharing-backend/src/main/java/com/godeltech/bikesharing/controller/ServiceOperationController.java
@@ -6,11 +6,14 @@ import com.godeltech.bikesharing.models.request.StartEquipmentMaintenanceRequest
 import com.godeltech.bikesharing.models.response.EquipmentMaintenanceResponse;
 import com.godeltech.bikesharing.models.response.FinishEquipmentMaintenanceResponse;
 import com.godeltech.bikesharing.service.EquipmentMaintenanceService;
+import java.util.List;
+import java.util.Optional;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +27,13 @@ public class ServiceOperationController {
   private final EquipmentMaintenanceService equipmentMaintenanceService;
   private final ServiceOperationMapper serviceOperationMapper;
 
+  @GetMapping
+  public ResponseEntity<List<EquipmentMaintenanceResponse>> getAllEquipmentMaintenances() {
+    return ResponseEntity.of(Optional.empty());
+  }
+
   @PostMapping("/start")
+//  FIXME rename to startMaintenanceService
   public ResponseEntity<EquipmentMaintenanceResponse> startRentOperation(
       @Valid @RequestBody StartEquipmentMaintenanceRequest request) {
     var serviceOperationModel = serviceOperationMapper.mapToModel(request);
@@ -33,7 +42,10 @@ public class ServiceOperationController {
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
+  // TODO The PUT method should be here. the similar as in RentOperationController
+//    @PutMapping("/{id}")
   @PostMapping("/finish")
+  //  FIXME rename to finishMaintenanceService
   public ResponseEntity<FinishEquipmentMaintenanceResponse> finishRentOperation(
       @Valid @RequestBody FinishEquipmentMaintenanceRequest request) {
     var serviceOperationModel = serviceOperationMapper.mapToModel(request);
--- bikesharing-backend/src/main/java/com/godeltech/bikesharing/models/enums/RentTimeUnit.java
+++ bikesharing-backend/src/main/java/com/godeltech/bikesharing/models/enums/RentTimeUnit.java
@@ -7,7 +7,7 @@ public enum RentTimeUnit {
   HALF_HOUR(30),
   HOUR(60),
   DAY(24 * 60);
-
+//TODO Let's try to use Duration class in this field. I think it will be more readable, understandable and flexible
   private final long durationInMin;
 
   RentTimeUnit(long durationInMin) {
